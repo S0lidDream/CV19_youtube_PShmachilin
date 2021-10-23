a6c7e9a23a458d37bbde93ea8e829daeddc67599
Artem Pshichko <brennend3956@gmail.com>
2021-10-23 21:22:47 +0600

    Добавляем модели

---
+++ CV19/Models/CountryInfo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+
+namespace CV19.Models
+{
+    internal class CountryInfo : PlaceInfo
+    {
+        public IEnumerable<ProvinceInfo> ProvinceCounts { get; set; }
+    }
+
+}
---
+++ CV19/Models/ProvinceInfo.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CV19.Models
+{
+    internal class ProvinceInfo : PlaceInfo
+    {
+
+    }
+}
