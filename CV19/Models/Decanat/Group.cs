0e199558703b9480bfb0bade6be8a57a90b1e2f8
Artem Pshichko <brennend3956@gmail.com>
2021-10-27 16:39:24 +0600

    Тестовая предметная область

---
+++ CV19/Models/Decanat/Group.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CV19.Models.Decanat
+{
+    internal class Group
+    {
+        public string Name { get; set; }
+
+        public ICollection<Student> Students { get; set; }
+
+
+    }
+}
