fb0bc0c9e3847ee0e7a934933de10d598bd251cd
Artem Pshichko <brennend3956@gmail.com>
2021-10-27 17:09:38 +0600

    Дизайн главного окна для работы со списком студентов

--- CV19/ViewModels/MainWindowViewModel.cs
+++ CV19/ViewModels/MainWindowViewModel.cs
@@ -1,16 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
 using System.Text;
 using System.Windows;
 using System.Windows.Input;
 using CV19.Infrastructure.Commands;
 using CV19.Models;
+using CV19.Models.Decanat;
 using CV19.ViewModels.Base;
 
 namespace CV19.ViewModels
 {
     internal class MainWindowViewModel : ViewModel
     {
+        /*-------------------------------------------------------------------------------------------*/
+
+        public ObservableCollection<Group> Groups { get; set; }
+
         #region SelectedPageIndex : int - Номер выбранной вкладки
         /// <summary>
         /// Номер выбранной вкладки
@@ -28,8 +35,6 @@ namespace CV19.ViewModels
 
         #endregion
 
-
-
         #region TestDataPoint : IEnumerable<DataPoint> - Тестовый набор данных для визууализации графиков
 
         private IEnumerable<DataPoint> _TestDataPoints;
@@ -40,7 +45,6 @@ namespace CV19.ViewModels
         }
 
         #endregion
-
 
         #region Заголовок главного окна
         private string _Title = "Анализ статистики CV19";
@@ -78,6 +82,8 @@ namespace CV19.ViewModels
         }
         #endregion
 
+        /*-------------------------------------------------------------------------------------------*/
+
         #region Команды
 
         #region CloseApplicationCommand
@@ -100,6 +106,7 @@ namespace CV19.ViewModels
         }
         #endregion
 
+        /*-------------------------------------------------------------------------------------------*/
         public MainWindowViewModel()
         {
             #region Команды
@@ -117,8 +124,24 @@ namespace CV19.ViewModels
                 data_points.Add(new DataPoint { XValue = x, YValue = y });
 
             }
-
             TestDataPoints = data_points;
+
+            int student_index = 1;
+            var students = Enumerable.Range(1, 10).Select(i => new Student
+            {
+                Name = $"Name {student_index}",
+                Surname = $"Surname {student_index}",
+                Patronymic = $"Patronymic {student_index++}",
+                Birthday = DateTime.Now,
+                Rating = 0
+            });
+            var groups = Enumerable.Range(1, 20).Select(i => new Group 
+            {
+                Name = $"Группа {i}",
+                Students = new ObservableCollection<Student>(students)
+            });
+            Groups = new ObservableCollection<Group>(groups);
+
         }
     }
 }
