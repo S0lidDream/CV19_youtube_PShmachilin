19de57ee6306450ba9ddf9bca9b5ccfdccc9193d
Artem Pshichko <brennend3956@gmail.com>
2021-12-21 19:51:37 +0600

    Мультиконвертер

---
+++ CV19/Infrastructure/Converters/ToArrayConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Windows.Data;
+
+namespace CV19.Infrastructure.Converters
+{
+    internal class ToArrayConverter : MultiConverter
+    {
+        public override object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            var collection = new CompositeCollection();
+            foreach(var value in values)
+            {
+                collection.Add(value);
+            }
+            return collection;
+        }
+
+        //public override object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        //{
+        //    return value as object[];
+        //}
+    }
+}
